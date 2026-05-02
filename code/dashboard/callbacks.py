e0a2e8f85ecef05fd7a99de046eba85909273d43
maelstrom <CephalopodMaster@protonmail.com>
2026-05-02 16:27:20 +0000

    Docstrings added

--- code/dashboard/callbacks.py
+++ code/dashboard/callbacks.py
@@ -1,13 +1,48 @@
-from dash import Input, Output
+# -*- coding: utf-8 -*-
+"""
+dashboard/callbacks.py
 
-def register_callbacks(app, dashboard):
+Callback for updating the figures on the dashboard with user input
+"""
+
+# built-in module imports
+from typing import Tuple
+
+# 3rd party module imports
+from dash import Dash, Input, Output
+from plotly.graph_objects import Figure
+
+# local module imports
+from dashboard import Dashboard
+
+def register_callbacks(app: Dash, dashboard: Dashboard):
+    """
+    Registers a callback function to respond when the input value on the
+    activity-type-dropdown changes, and triggering the output (figures) to
+    update, and then returning the updated figure objects to render.
+
+    Args:
+        app (dash.Dash): Dash application from dash library, which is the
+            actual application that is serving contents.
+        dashboard (Dashboard): Locally defined Dashboard object that holds the
+            figure objects as its attributes.
+    """
     @app.callback(
         Output("progress-bar", "figure"),
         Output("line-plot", "figure"),
         Output("cumulative-line-plot", "figure"),
         Input("activity-type-dropdown", "value"),
     )
-    def update_charts(activity_type):
+    def update_charts(activity_type:str) -> tuple[Figure, Figure, Figure]:
+        """
+        When the activity type is selected, return the graphic objects.
+
+        Args:
+            activity_type (str): String name of the activity.
+        Return:
+            tuple[Figure, Figure, Figure]: Tuple of dashboard objects from the
+            figure.
+        """
         return (
             dashboard.progress_bar(activity_type),
             dashboard.lineplot(activity_type),
--- code/dashboard/dashboard.py
+++ code/dashboard/dashboard.py
@@ -1,17 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+dashboard.py
+"""
+
+# 3rd-party module imports
+from plotly.graph_objects import Figure
+
+# local module imports
+from core.chart_service import ChartService
+from core.layout_service import LayoutService
+
 class Dashboard:
-    def __init__(self, layout_service, chart_service):
+    def __init__(
+            self,
+            layout_service: LayoutService,
+            chart_service: ChartService
+        ):
+        """
+        Dashboard object that holds all the relevant parts under one namespace.
+
+        Args:
+            layout_service (LayoutService): For defining the structure of the
+                dashboard.
+            chart_service (ChartService): For defining the types of charts to
+                render on the dashboard.
+
+        Attrs:
+            ibid.
+        """
         self.layout_service = layout_service
         self.chart_service = chart_service
 
     def layout(self):
+        """
+        Builds the layout. See core/layout_service.
+        """
         return self.layout_service.build()
 
-    def progress_bar(self, activity_type):
+    def progress_bar(self, activity_type: str) -> Figure:
+        """
+        Render a progress bar for given activity type.
+
+        Args:
+            activity_type (str): Name of activity to plot.
+
+        Returns:
+            plotly.graphicObjects.Figure: Figure containing a progress bar.
+        """
         return self.chart_service.progress_bar(activity_type)
 
-    def lineplot(self, activity_type):
+    def lineplot(self, activity_type: str) -> Figure:
+        """
+        Render a lineplot for given activity type.
+
+        Args:
+            activity_type (str): Name of activity to plot.
+
+        Returns:
+            plotly.graphicObjects.Figure: Figure containing a line plot.
+        """
         return self.chart_service.lineplot(activity_type)
 
-    def cumlineplot(self, activity_type):
+    def cumlineplot(self, activity_type: str) -> Figure:
+        """
+        Render a cumulative plot of progress for given activity type.
+
+        Args:
+            activity_type (str): Name of activity to plot.
+
+        Returns:
+            plotly.graphicObjects.Figure: Figure containing a cumulative plot.
+        """
         return self.chart_service.cumlineplot(activity_type)
 
